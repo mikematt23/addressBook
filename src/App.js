7e3b00124fa9beffa92785bbbbcf37e3a88bfdb5
Michael MAtteis <michaelcmatteis@gmail.com>
2020-08-11 19:23:45 -0500

    seting up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      users :[]
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://randomuser.me/api?results=25')
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+            this.setState({users: data})
+        })
+  }
+
+  render(){
+    return(
+      <div></div>
+    )
+  }
+  
 }
+
 
 export default App;
